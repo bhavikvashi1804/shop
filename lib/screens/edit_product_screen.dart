657c6000705e884de1aab6961b981e1b76bfbe98
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-03-20 12:19:55 +0530

    Form Validation

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -54,7 +54,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _updateImageUrl(){
 
-    if(!_imageUrlFocusNode.hasFocus){
+    if(_imageUrlFocusNode.hasFocus){
+      
+      //now make sure that if url is not valid then we donot want to display image
+      //if url is invalid image display URL will remain as it is
+      if( (!_imageUrlController.text.startsWith('http') && !_imageUrlController.text.startsWith('https'))||
+      (!_imageUrlController.text.endsWith('.png')&& !_imageUrlController.text.endsWith('.jpeg')&& !_imageUrlController.text.endsWith('.jpg'))){
+        return ;
+      }
+      
+      
       // if it loses focus then set state means widgets are rebuild and image preview display container has image 
       setState(() {
         
@@ -66,6 +75,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
 
   void _saveForm(){
+
+    final isValid=_form.currentState.validate();
+   
+
+    if(!isValid){
+      return;
+      //is it is not valid then simply exit
+      //else save the form
+    }
     _form.currentState.save();
 
 
@@ -107,6 +125,14 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_){
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                validator: (value){
+                  if(value.isEmpty){
+                    return 'Please provide title.';
+                  }
+                  return null;
+                  //return null means no error
+
+                },
                 onSaved: (value){
                   _editedProduct=Product(
                     id: null ,
@@ -124,6 +150,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_){
                   FocusScope.of(context).requestFocus(_descFocusNode);
+                },
+                validator: (value){
+                  if(value.isEmpty){
+                    return 'Please provide price.';
+                  }
+                  if(double.tryParse(value)==null){
+                    return 'Please provide valid number.';
+                  }
+                  if(double.parse(value)<=0){
+                    return 'Please enter price which is greater than 0.';
+                  }
+                  return null;
+                  //return null means no error
+
                 },
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
@@ -146,6 +186,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 //here we have enter button to enter new line
                 //show we do not need to provide to pass the focus to next input
                 focusNode: _descFocusNode,
+                validator: (value){
+                  if(value.isEmpty){
+                    return 'Please enter a description.';
+                  }
+                  if(value.length<10){
+                    return 'Description should be atleast 10 character long.';
+                  }
+
+                  return null;
+                },
                 onSaved: (value){
                   _editedProduct=Product(
                     id: null ,
@@ -188,6 +238,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       keyboardType: TextInputType.url,
                       textInputAction: TextInputAction.done,
                       controller: _imageUrlController,
+                      validator: (value){
+                        if(value.isEmpty){
+                          return 'Please enter image URL.';
+                        }
+                        if(!value.startsWith('http') && !value.startsWith('https')){
+                          return 'Please enter valid URL.';
+                        }
+                        if(!value.endsWith('.png') && !value.endsWith('.jpg') && !value.endsWith('jpeg')){
+                          return 'Please enter valid image URL.';
+                        }
+                        return null;
+                      },
                       focusNode: _imageUrlFocusNode,
                       //now we want that when user click on other field thwn it also show image preview
                       //as of noew when user press submit then and only then image is displayed
