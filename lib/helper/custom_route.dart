ae30eabe4421ede7b9ec5ff9164af6e9d3b47185
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-03-23 12:45:18 +0530

    Scale Transition

--- lib/helper/custom_route.dart
+++ lib/helper/custom_route.dart
@@ -5,11 +5,16 @@ class CustomPageTransitionBuilder extends PageTransitionsBuilder{
   @override
   Widget buildTransitions<T>(PageRoute<T> route, BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
     
-    return SlideTransition(
-      position: Tween<Offset>(
-        begin: const Offset(-1, 0),
-        end: Offset.zero,
-      ).animate(animation),
+    return ScaleTransition(
+      scale: Tween<double>(
+        begin: 0.0,
+        end: 1.0,
+      ).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
       child: child,
     );
   }
