485c98f3964d49dd50d1d410ac595b55b2329245
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-03-23 10:44:50 +0530

    Animated Container with Orders also we need to asign AC to Card and check the height of card at here

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -20,64 +20,70 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10.0),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(
-              '\$ ${widget.order.amount}'
-            ),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(expanded?Icons.expand_less:Icons.expand_more), 
-              onPressed: (){
-                setState(() {
-                  expanded=!expanded;
-                });
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: expanded?min(widget.order.products.length*20.0 +110.0,200.0):95,
+      // remeber this 
+      child: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                '\$ ${widget.order.amount}'
+              ),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(expanded?Icons.expand_less:Icons.expand_more), 
+                onPressed: (){
+                  setState(() {
+                    expanded=!expanded;
+                  });
 
-              }
+                }
+              ),
             ),
-          ),
-          if(expanded)Container(
-            padding: EdgeInsets.symmetric(
-              horizontal:16,
-              vertical: 4,
-            ),
-            height:min(widget.order.products.length*20.0 +10.0,100.0),
-            child:ListView(
-              children: widget.order.products.map(
-                (prod)=> Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      prod.title,
-                      style:TextStyle(
-                        fontSize:18,
-                        fontWeight:FontWeight.bold,
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              padding: EdgeInsets.symmetric(
+                horizontal:16,
+                vertical: 4,
+              ),
+              height:expanded?min(widget.order.products.length*20.0 +10.0,100.0):0,
+              child:ListView(
+                children: widget.order.products.map(
+                  (prod)=> Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        prod.title,
+                        style:TextStyle(
+                          fontSize:18,
+                          fontWeight:FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text(
-                      '${prod.quantity}x \$ ${prod.price}',
-                      style:TextStyle(
-                        fontSize:18,
-                        color:Colors.grey,
+                      Text(
+                        '${prod.quantity}x \$ ${prod.price}',
+                        style:TextStyle(
+                          fontSize:18,
+                          color:Colors.grey,
+                        ),
+
                       ),
 
-                    ),
+                    ],
+                  )
 
-                  ],
-                )
+                ).toList(),
+              )
 
-              ).toList(),
-            )
-
-          ),
-        ],
+            ),
+          ],
+        ),
+        
       ),
-      
     );
   }
 }
