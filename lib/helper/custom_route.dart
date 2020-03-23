2663f4e2bc8200e5f79688269fba45ffb77b9aa2
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-03-23 13:03:29 +0530

    Scale Transition Another Way

--- lib/helper/custom_route.dart
+++ lib/helper/custom_route.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-
-class CustomPageTransitionBuilder extends PageTransitionsBuilder{
+class CustomPageTransitionBuilder extends PageTransitionsBuilder {
   @override
-  Widget buildTransitions<T>(PageRoute<T> route, BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-    
+  Widget buildTransitions<T>(
+      PageRoute<T> route,
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
     return ScaleTransition(
       scale: Tween<double>(
         begin: 0.0,
@@ -18,5 +21,5 @@ class CustomPageTransitionBuilder extends PageTransitionsBuilder{
       child: child,
     );
   }
-
 }
+
