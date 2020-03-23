b280bb94ba9b0e243ebf0c723bcd4fdff7d3a371
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-03-23 12:44:11 +0530

    Slide Right Transition

--- lib/helper/custom_route.dart
+++ lib/helper/custom_route.dart
@@ -5,12 +5,12 @@ class CustomPageTransitionBuilder extends PageTransitionsBuilder{
   @override
   Widget buildTransitions<T>(PageRoute<T> route, BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
     
-    return ScaleTransition(
-      scale: animation,
-      child: RotationTransition(
-        turns: animation,
-        child: child,
-      ),
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: Offset.zero,
+      ).animate(animation),
+      child: child,
     );
   }
 
